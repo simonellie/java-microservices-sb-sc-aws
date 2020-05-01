3079066ce45ec1a06b2fa3f582ae063a3344179c
Simonelli Emanuele <simonellie@users.noreply.github.com>
2020-05-01 18:42:29 +0200

    Section 3 - lessons from 20 Services

---
+++ articoli-web-service/src/main/java/com/xantrix/webapp/service/ItemServiceImpl.java
@@ -0,0 +1,44 @@
+package com.xantrix.webapp.service;
+
+import com.xantrix.webapp.entity.Item;
+import com.xantrix.webapp.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class ItemServiceImpl implements ItemService {
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Override
+    public List<Item> SelByDescrizione(String descrizione) {
+        return itemRepository.findByDescrizioneLike(descrizione);
+    }
+
+    @Override
+    public Item SelByCodArt(String codArt) {
+        return itemRepository.findByCodArt(codArt);
+    }
+
+    @Override
+    public Item SelByBarcode(String barcode) {
+        return itemRepository.SelByEan(barcode);
+    }
+
+    @Override
+    @Transactional
+    public void deleteItem(Item item) {
+        itemRepository.delete(item);
+    }
+
+    @Override
+    @Transactional
+    public void insertItem(Item item) {
+        itemRepository.save(item);
+    }
+}
